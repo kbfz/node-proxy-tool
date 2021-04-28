9c09d0f44ad90df3b3df0000aa3e13b45de4c096
liujia <993654866@qq.com>
2021-04-28 10:24:18 +0800

    1

---
+++ bin/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+
+const program = require('commander');
+const chalk = require('chalk');
+
+const version = require('../package.json').version // 当前的版本
+
+program
+    .version(chalk.green(version)) // 当前的版本
+    .usage('<command> [options]')
+
+program
+    .command('init <origin> <port>') // 命令 
+    .description("init <origin> <port>(要代理的地址和端口)") // 当前命令的说明 命令的描述
+    .action(require('../lib/init'))  // 执行命令要做的事 
+
+program.parse(process.argv) // 主进程的参数 获取命令的参数
